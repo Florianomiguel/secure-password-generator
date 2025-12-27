c3398d50d228cc29ac7e4a81a0a9767c2e767b51
Miguel <florianooxz@gmail.com>
2025-12-27 00:16:17 -0300

    Implement console clear function in password manager
    Added a function to clear the console screen before displaying the menu.

--- Password_generator/main.py
+++ Password_generator/main.py
@@ -1,21 +1,24 @@
+import os
 import random_password
 from colorama import Fore, Style, init
 
 init(autoreset=True)
 
+def cleam_scream():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 while True:
-    print(f"\n{'-'*30}")
+    cleam_scream()
+    print(f"{'-'*30}")
     print(f"{Fore.MAGENTA}{Style.BRIGHT}PASSWORD MANAGER MENU")
     print(f"{'-'*30}")
     
     try:
-        
         prompt = (f"Do you want to:\n"
                   f"{Fore.YELLOW}[1]{Fore.RESET} Create a new password\n"
                   f"{Fore.YELLOW}[2]{Fore.RESET} Check current password\n"
                   f"{Fore.RED}[0]{Fore.RESET} Exit\n"
-                  f"{Fore.CYAN}Choose an option: ")
-        
+                  f"{Fore.CYAN}Choose an option: ")     
         option = int(input(prompt))
 
         if option == 1:
@@ -24,16 +27,23 @@ while True:
             length = int(input(length_prompt))
             random_password.generate_password(length)
             
+            input(f"\n{Fore.CYAN}Press Enter to return to menu...")
+            
         elif option == 2:
             pass_prompt = f"{Fore.GREEN}Enter your password to check: "
             user_password = input(pass_prompt).strip()
             random_password.try_level(user_password)
             
+            input(f"\n{Fore.CYAN}Press Enter to return to menu...")
+            
         elif option == 0:
             print(f"\n{Fore.LIGHTBLACK_EX}Exiting... Goodbye!")
             break  
+            
         else:
             print(f"{Fore.RED}Invalid choice! Please enter 0, 1, or 2.")
+            input(f"\n{Fore.CYAN}Press Enter to try again...")
             
     except ValueError:
         print(f"{Fore.RED}{Style.BRIGHT}ERROR! Please enter numbers only.")
+        input(f"\n{Fore.CYAN}Press Enter to try again...")
